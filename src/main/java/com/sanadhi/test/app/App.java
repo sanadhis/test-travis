ec91ec3a24bf0132a35030cb03f5a2bd1d539f23
sanadhis <sanadhis@gmail.com>
2018-06-19 22:42:18 +0200

    --Change Notification setup

--- src/main/java/com/sanadhi/test/app/App.java
+++ src/main/java/com/sanadhi/test/app/App.java
@@ -9,5 +9,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println(hello());
+    }
+
+    public static String hello(){
+        return "Bonjour!";
     }
 }
